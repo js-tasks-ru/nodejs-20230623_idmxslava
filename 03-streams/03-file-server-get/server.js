1282f3bdac3c6a7c96ace2c76655e779693d778a
idmx <slavyan1507@mail.ru>
2023-06-29 15:47:15 +0300

    added solutions for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,23 +1,40 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+const sendError = (res) => (message, code) => {
+  res.statusCode = code;
+  res.end(message);
+};
+
+server.on("request", (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
+  const outStream = fs.createReadStream(filepath);
+
+  const sendErrorMessage = sendError(res);
 
   switch (req.method) {
-    case 'GET':
-
+    case "GET":
+      if (pathname.includes("/")) {
+        sendErrorMessage("bad request", 400);
+      }
+      outStream.pipe(res);
+      outStream.on("error", (error) => {
+        if (error.code === "ENOENT") {
+          sendErrorMessage("file not found", 404);
+        }
+      });
+      req.on("aborted", () => outStream.destroy());
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      sendErrorMessage("Not implemented", 501);
   }
 });
 
