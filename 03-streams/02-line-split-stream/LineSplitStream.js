d283969d9acb2f62146dde6fe51340bdc19f44c2
idmx <slavyan1507@mail.ru>
2023-06-29 14:35:57 +0300

    add solutions for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,26 @@
-const stream = require('stream');
-const os = require('os');
+const stream = require("stream");
+const os = require("os");
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.lastSlice = "";
   }
 
   _transform(chunk, encoding, callback) {
+    const formattedChunk = this.lastSlice + chunk.toString(this.encoding);
+    const splittedChunk = formattedChunk.split(os.EOL);
+    this.lastSlice = splittedChunk.pop();
+
+    for (const slicedChunk of splittedChunk) {
+      this.push(slicedChunk);
+    }
+    callback(null);
   }
 
   _flush(callback) {
+    this.lastSlice && this.push(this.lastSlice);
+    callback(null);
   }
 }
 
