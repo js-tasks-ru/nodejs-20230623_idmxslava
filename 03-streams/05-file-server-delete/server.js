cd406960bae0d2b0676ebc55b1714f3ec6fd1ce5
idmx <slavyan1507@mail.ru>
2023-06-30 08:51:00 +0300

    added solutions for 3-module 5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,23 +1,40 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const { sendMessage } = require("../../helpers/sendMessage.js");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
+  const sendCodeMessage = sendMessage(res);
 
   switch (req.method) {
-    case 'DELETE':
+    case "DELETE":
+      try {
+        if (pathname.includes("/")) {
+          sendCodeMessage("bad request", 400);
+          break;
+        }
+        if (!fs.existsSync(filepath)) {
+          sendCodeMessage("file not found", 404);
+          break;
+        }
 
+        fs.unlinkSync(filepath);
+
+        sendCodeMessage("ok", 200);
+      } catch (e) {
+        sendCodeMessage("Something went wrong", 500);
+      }
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      sendCodeMessage("Not implemented", 501);
   }
 });
 
