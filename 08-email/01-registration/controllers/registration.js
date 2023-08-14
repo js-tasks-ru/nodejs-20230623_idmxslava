ee4a028d95ac21bfa318a7f80c71a0e37fc01737
idmx <slavyan1507@mail.ru>
2023-08-14 15:38:34 +0300

    added solution for 8-module 1-task

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,43 @@
-const { v4: uuid } = require('uuid');
-const User = require('../models/User');
-const sendMail = require('../libs/sendMail');
+const { v4: uuid } = require("uuid");
+const User = require("../models/User");
+const sendMail = require("../libs/sendMail");
 
 module.exports.register = async (ctx, next) => {
-
+  const token = uuid();
+  const { email, displayName, password } = ctx.request.body;
+  try {
+    const user = await User.create({
+      email,
+      displayName,
+      verificationToken: token,
+    });
+    await user.setPassword(password);
+    await user.save();
+    await sendMail({
+      template: "confirmation",
+      locals: { token },
+      to: email,
+      subject: "Подтвердите почту",
+    });
+    ctx.body = { status: "ok" };
+  } catch (e) {
+    ctx.throw(400, e);
+  }
 };
 
 module.exports.confirm = async (ctx, next) => {
-
+  try {
+    const user = await User.findOne({
+      verificationToken: ctx.request.body.verificationToken,
+    });
+    if (!user) {
+      ctx.throw(404, "Ссылка подтверждения недействительна или устарела");
+    }
+    user.verificationToken = undefined;
+    await user.save();
+    const token = uuid();
+    ctx.body = { token };
+  } catch (e) {
+    ctx.throw(400, e);
+  }
 };
